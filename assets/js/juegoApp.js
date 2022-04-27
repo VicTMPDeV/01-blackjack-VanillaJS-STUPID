1a01840715429a1272ae10db1d742b8580d80b08
Victor_Morales_Perez <vmorales@atsistemas.com>
2022-04-27 17:18:53 +0200

    computer logic implementation and output messages at the end

--- assets/js/juegoApp.js
+++ assets/js/juegoApp.js
@@ -16,10 +16,13 @@ const specialCards = ['A','J','Q','K'];
 let playerScore = 0 , computerScore = 0;
 
 //HTML ref variables
-const btnHit = document.querySelector('#btnHitCard');
-const playerScoreTag = document.querySelector('#playerScoreTag'); 
+const btnHit   = document.querySelector('#btnHitCard');
+const btnStand = document.querySelector('#btnStand');
+
+const playerScoreTag   = document.querySelector('#playerScoreTag'); 
 const computerScoreTag = document.querySelector('#computerScoreTag');
-const divPlayerCards = document.querySelector('#player-cards'); 
+
+const divPlayerCards   = document.querySelector('#player-cards'); 
 const divComputerCards = document.querySelector('#computer-cards'); 
 
 const createDeck = () => {
@@ -38,7 +41,7 @@ const createDeck = () => {
             deck.push(special + type);
         }
     }
-    deck = _.shuffle(deck);
+    deck = _.shuffle(deck);//underscore function
     deckSize = deck.length;
     return deck;
 }
@@ -92,10 +95,68 @@ btnHit.addEventListener('click', () => {
 
     if(playerScore > 21){
         console.warn('HAS PERDIDO');
-        btnHit.disabled = true;
+        btnHit.disabled   = true;
+        btnStand.disabled = true;
+        ComputerPlay(playerScore);
     } else if (playerScore === 21) {
         console.warn('JUGADOR: 21 PUNTOS!!!');
+        btnHit.disabled   = true;
+        btnStand.disabled = true;
+        ComputerPlay(playerScore);
     }
 
 })
 
+btnStand.addEventListener('click',() => {
+    //Bloqueo las acciones del jugador
+    btnHit.disabled   = true;
+    btnStand.disabled = true;
+    //Juega la máquina
+    ComputerPlay(playerScore);
+})
+
+//Machine (Computer player) logic -> minScoreToWin = playerScore
+const ComputerPlay = (minScoreToWin) => {
+
+    /*El jugador, como mínimo, pedirá 1 o 0 cartas antes de plantarse
+    , por tanto, la máquina para ganar tendrá que pedir 1 carta por lo 
+    menos */
+    while( (computerScore < minScoreToWin) && (minScoreToWin <= 21) ) {
+
+        const hittedCard = hitCard();
+        computerScore = computerScore + cardValue(hittedCard);
+        computerScoreTag.innerHTML = computerScore;
+
+        //<img class="deck-card" src="assets/cards/2C.png">
+        const imgCard = document.createElement('img');
+        imgCard.src = `assets/cards/${hittedCard}.png`;
+        imgCard.classList.add('deck-card')
+        divComputerCards.append(imgCard);
+
+        if (minScoreToWin > 21) {
+            break;
+        } 
+
+    }
+
+    setTimeout(() => {
+        if ( computerScore === minScoreToWin ){
+            alert('EMPATE');
+        } else if ( minScoreToWin > 21){
+            alert('COMPUTADORA GANA');
+        } else if (computerScore > 21){
+            alert('GANASTE!!!');
+        } else if (playerScore > computerScore) {
+            alert('GANASTE!!!');
+        } else if (computerScore > playerScore) {
+            alert('COMPUTADORA GANA');
+        } else {
+            alert('COMPUTADORA GANA');
+        }
+    }, 10);
+    
+
+}
+//Testing Machine Logic
+// console.log(`puntos jugador ${12}`)
+// ComputerPlay(12);
